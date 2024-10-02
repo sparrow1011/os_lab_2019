4ca9b79e45d15c61b1f43a866297d9d58cab855c
sparrow1011 <994738@gmail.com>
2024-10-02 10:31:25 +0000

    добавлены commands_lab3.txt, find_min_max.c, makefile, parallel_min_max.c, run_sequential_min_max.c

--- lab3/src/parallel_min_max.c
+++ lab3/src/parallel_min_max.c
@@ -40,24 +40,30 @@ int main(int argc, char **argv) {
         switch (option_index) {
           case 0:
             seed = atoi(optarg);
-            // your code here
-            // error handling
+            if (seed <= 0) {
+              printf("seed is a positive number\n");
+              return 1;
+            }
             break;
           case 1:
             array_size = atoi(optarg);
-            // your code here
-            // error handling
+            if (array_size <= 0) {
+              printf("array_size is a positive number\n");
+              return 1;
+            }
             break;
           case 2:
             pnum = atoi(optarg);
-            // your code here
-            // error handling
+            if (pnum <= 0) {
+              printf("pnum is a positive number\n");
+              return 1;
+            }
             break;
           case 3:
             with_files = true;
             break;
 
-          defalut:
+          default:
             printf("Index %d is out of options\n", option_index);
         }
         break;
@@ -91,6 +97,13 @@ int main(int argc, char **argv) {
   struct timeval start_time;
   gettimeofday(&start_time, NULL);
 
+  int chunk_size = array_size / pnum;
+  int **pipes = malloc(pnum * sizeof(int *));
+  for (int i = 0; i < pnum; i++) {
+    pipes[i] = malloc(2 * sizeof(int));
+    pipe(pipes[i]);
+  }
+
   for (int i = 0; i < pnum; i++) {
     pid_t child_pid = fork();
     if (child_pid >= 0) {
@@ -98,13 +111,22 @@ int main(int argc, char **argv) {
       active_child_processes += 1;
       if (child_pid == 0) {
         // child process
-
-        // parallel somehow
+        int begin = i * chunk_size;
+        int end = (i == pnum - 1) ? array_size : begin + chunk_size;
+        struct MinMax min_max = GetMinMax(array, begin, end);
 
         if (with_files) {
           // use files here
+          char filename[20];
+          sprintf(filename, "result_%d.txt", i);
+          FILE *file = fopen(filename, "w");
+          fprintf(file, "%d %d\n", min_max.min, min_max.max);
+          fclose(file);
         } else {
           // use pipe here
+          close(pipes[i][0]); // close read end
+          write(pipes[i][1], &min_max, sizeof(min_max));
+          close(pipes[i][1]); // close write end
         }
         return 0;
       }
@@ -116,8 +138,7 @@ int main(int argc, char **argv) {
   }
 
   while (active_child_processes > 0) {
-    // your code here
-
+    wait(NULL);
     active_child_processes -= 1;
   }
 
@@ -131,13 +152,29 @@ int main(int argc, char **argv) {
 
     if (with_files) {
       // read from files
+      char filename[20];
+      sprintf(filename, "result_%d.txt", i);
+      FILE *file = fopen(filename, "r");
+      fscanf(file, "%d %d", &min, &max);
+      fclose(file);
     } else {
       // read from pipes
+      close(pipes[i][1]); // close write end
+      struct MinMax temp_min_max;
+      read(pipes[i][0], &temp_min_max, sizeof(temp_min_max));
+      close(pipes[i][0]); // close read end
+      min = temp_min_max.min;
+      max = temp_min_max.max;
     }
 
     if (min < min_max.min) min_max.min = min;
     if (max > min_max.max) min_max.max = max;
   }
+
+  for (int i = 0; i < pnum; i++) {
+    free(pipes[i]);
+  }
+  free(pipes);
 
   struct timeval finish_time;
   gettimeofday(&finish_time, NULL);
